0dc196d03ed0cf5e857f07469fd59579959fa674
CarlosIUSalazar <carlosiusalazar@gmail.com>
2020-04-06 20:01:18 +0900

    Fixed errors

--- src/loscore.js
+++ src/loscore.js
@@ -1,4 +1,4 @@
-console.log("Hello World");
+//console.log("Hello World");
 // Let's make an object and start adding methods to it!
 class LoScore {
   identity(val) {
@@ -22,6 +22,7 @@ class LoScore {
     return ret_arr;
   }
 
+  
   /**
   | COLLECTIONS
   |~~~~~~~~~~
@@ -75,17 +76,32 @@ class LoScore {
 
   every(collection, test) {
     // YOUR CODE HERE
-    return this.reduce(
-      collection,
-      (boolean, item) => {
-        if (!boolean) {
-          return false;
+    if (collection.length == 0) {
+      return true;
+    }
+    if (test === undefined) {
+      for (let i of collection) {
+        if (i) {
+          return true;
         }
-        if (typeof test === "undefined") return item;
-        return test(item);
+        return false;
+      }
+    }
+    let bool = this.reduce(
+      collection,
+      (accumulator, value) => {
+        accumulator.push(test(value));
+        return accumulator;
       },
-      true
+      []
     );
+    let check = true;
+    for (let i of bool) {
+      if (!i) {
+        check = false;
+      }
+    }
+    return check;
   }
 
   /**
